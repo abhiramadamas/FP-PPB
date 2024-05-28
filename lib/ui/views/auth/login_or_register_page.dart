e040f84615fbde59e41ccce263df0b3ef80410f9
abhiramadamas <muhammaddamas81@gmail.com>
2024-05-28 10:42:14 +0700

    [feat] auth feature

---
+++ lib/ui/views/auth/login_or_register_page.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:mitchkoko_firebaseauth/pages/login_page.dart';
+import 'package:mitchkoko_firebaseauth/pages/register_page.dart';
+
+class LoginOrRegisterPage extends StatefulWidget {
+  const LoginOrRegisterPage({super.key});
+
+  @override
+  State<LoginOrRegisterPage> createState() => _LoginOrRegisterPageState();
+}
+
+class _LoginOrRegisterPageState extends State<LoginOrRegisterPage> {
+  //initially show login page
+  bool showLoginPage = true;
+
+  // toggle between login and register page
+  void togglePages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(onTap: togglePages);
+    } else {
+      return RegisterPage(onTap: togglePages);
+    }
+  }
+}
