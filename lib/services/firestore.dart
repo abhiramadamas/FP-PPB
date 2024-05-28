c249935c670aeb21c0e7d5b51af631e3d3ac6ed4
hasimiali <alihasimialihasimi@gmail.com>
2024-05-28 15:19:25 +0700

    Create usulan TA

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -3,6 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirestoreService {
 //   Get collection data
   final CollectionReference bimbingans = FirebaseFirestore.instance.collection('bimbingans');
+  final CollectionReference tugasAkhir = FirebaseFirestore.instance.collection('tugasAkhir');
 
 // Create
   Future<void> tambahBimbingan(String bimbingan){
@@ -21,4 +22,13 @@ class FirestoreService {
 // UPDATE
 
 // DELETE
+  Future<void> tambahTugasAkhir(String judul, String rencana, String pembimbing){
+    return bimbingans.add({
+      'judul': judul,
+      'rencana' : rencana,
+      'pembimbing' : pembimbing,
+      'timestamp': Timestamp.now(),
+    });
+  }
+
 }
