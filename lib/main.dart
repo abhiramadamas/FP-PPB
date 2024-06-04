8dd881823d3a5c995d6bee71763ea6ada57b74ab
Mohamad Kholid Bughowi <kholidbughowi@gmail.com>
2024-06-04 13:51:46 +0700

    feat: setting firestore persistence enabled

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:logprota/ui/components/main_navigation.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -7,6 +8,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
   );
   runApp(const MyApp());
 }
