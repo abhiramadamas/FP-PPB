5d455ddfe689f9add681516673b58ca4325d31b5
hasimiali <alihasimialihasimi@gmail.com>
2024-06-04 12:53:56 +0700

    view, update, and delete

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -6,6 +6,34 @@ class FirestoreService {
   final CollectionReference tugasAkhir = FirebaseFirestore.instance.collection('tugasAkhir');
 
 // Create
+  Future<void> tambahTugasAkhir(String judul, String rencana, String pembimbing){
+    return tugasAkhir.add({
+      'judul': judul,
+      'rencana' : rencana,
+      'pembimbing' : pembimbing,
+      'timestamp': Timestamp.now(),
+    });
+  }
+// READ
+  Stream<QuerySnapshot> getTugasAkhirStream() {
+    final bimbinganStream = tugasAkhir.orderBy('timestamp', descending: true).snapshots();
+
+    return bimbinganStream;
+  }
+// UPDATE
+  Future<void> updateTugasAkhir(String docID, String newJudul, String newRencana, String newPembimbing){
+    return tugasAkhir.doc(docID).update({
+      'judul': newJudul,
+      'rencana' : newRencana,
+      'pembimbing' : newPembimbing,
+      'timestamp': DateTime.now()
+    });
+  }
+// DELETE
+  Future<void> deleteTugasAkhir(String docID){
+    return tugasAkhir.doc(docID).delete();
+  }
+
   Future<void> tambahBimbingan(String bimbingan){
     return bimbingans.add({
       'note': bimbingan,
@@ -13,22 +41,10 @@ class FirestoreService {
     });
   }
 
-// READ
   Stream<QuerySnapshot> getBimbinganStream() {
     final bimbinganStream = bimbingans.orderBy('timestamp', descending: true).snapshots();
 
     return bimbinganStream;
-  }
-// UPDATE
-
-// DELETE
-  Future<void> tambahTugasAkhir(String judul, String rencana, String pembimbing){
-    return bimbingans.add({
-      'judul': judul,
-      'rencana' : rencana,
-      'pembimbing' : pembimbing,
-      'timestamp': Timestamp.now(),
-    });
   }
 
 }
