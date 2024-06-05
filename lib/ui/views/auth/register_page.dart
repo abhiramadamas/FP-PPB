0a64a04c09e5626a016857c750672ba92e8db886
abhiramadamas <muhammaddamas81@gmail.com>
2024-06-05 20:29:33 +0700

    feat auth and register

--- lib/ui/views/auth/register_page.dart
+++ lib/ui/views/auth/register_page.dart
@@ -1,9 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:mitchkoko_firebaseauth/components/my_button.dart';
-import 'package:mitchkoko_firebaseauth/components/my_textfield.dart';
-import 'package:mitchkoko_firebaseauth/components/square_tile.dart';
+import 'package:logprota/ui/components/my_button.dart';
+import 'package:logprota/ui/components/my_textfield.dart';
+import 'package:logprota/ui/components/square_tile.dart';
 
 class RegisterPage extends StatefulWidget {
   final Function()? onTap;
@@ -172,12 +172,12 @@ class _RegisterPageState extends State<RegisterPage> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: const [
                     // google button
-                    SquareTile(imagePath: 'lib/images/google.png'),
+                    SquareTile(imagePath: 'image/google.png'),
 
                     SizedBox(width: 25),
 
                     // apple button
-                    SquareTile(imagePath: 'lib/images/apple.png')
+                    SquareTile(imagePath: 'image/apple.png')
                   ],
                 ),
 
